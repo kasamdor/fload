4aa8a45920e460f6bf60e74d1d527476ef33d5a9
Fedor Kapralov <53122450+kasamdor@users.noreply.github.com>
2022-08-08 16:10:25 +0300

    Delete counting of rows number
    Last empty strings in file are now processed correctly

--- fload.m
+++ fload.m
@@ -4,7 +4,7 @@
 % * hl - number of header lines to ignore
 % Output:
 % * a  - double array
-function [a] = fload(fp, hl)
+function a = fload(fp, hl)
     %% arguments check 
     assert(hl >= 0, 'Number of header lines is incorrect.');
     assert(exist(fp, 'file') > 0, 'File %s does not exist.', fp);
@@ -12,22 +12,15 @@ function [a] = fload(fp, hl)
     fid = fopen(fp, 'r');
     assert(fid ~= -1, 'File %s can not be read.', fp);
     cuo = onCleanup(@() fclose(fid));
-    %% total file lines
-    tfl = 0;
-    while ~feof(fid)
-        tfl = tfl + sum(fread(fid, 16384, 'char') == char(10));
-    end
-    assert(tfl > hl, 'Allowed header lines number is exceeded.');
     %% columns number
-    frewind(fid);
     % skipping of header lines
     for k = 1:hl, fgetl(fid); end
     tline = fgetl(fid);
     [~, acols] = sscanf(tline, '%f');
+    data_wo_blanks = reshape(cell2mat(textscan(fid, '%f')), acols, [])';
     %% memory pre-allocation for array
-    arows = tfl - hl; 
-    a = zeros(arows, acols);
+    a = zeros(size(data_wo_blanks,1)+1, acols);
     %% array formation
-    a(1,:) = sscanf(tline, '%f');
-    a(2:end,:) = reshape(cell2mat(textscan(fid, '%f')), acols, [])';
+    a(1,:)     = sscanf(tline, '%f');
+    a(2:end,:) = data_wo_blanks;
 end

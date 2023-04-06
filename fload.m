9e173debaf8c20fc02f620011dfd4749817a3801
Fedor Kapralov <53122450+kasamdor@users.noreply.github.com>
2023-04-06 23:48:49 +0000

    Make number of header lines an optional parameter
    If argument `hl` is not specified, the function detect it automatically (thanks to @a-kozlov-msu).

--- fload.m
+++ fload.m
@@ -1,26 +1,22 @@
-% The function is a much faster analog of default MATLAB load function. 
-% Inputs:
-% * fp - file path
-% * hl - number of header lines to ignore
-% Output:
-% * a  - double array
-function a = fload(fp, hl)
-    %% arguments check 
-    assert(hl >= 0, 'Number of header lines is incorrect.');
-    assert(exist(fp, 'file') > 0, 'File %s does not exist.', fp);
-    %% file opening
+% The function is much faster analog of default `load` MATLAB function.
+% `fp` - file path, `hl` - header lines to skip (optional argument).
+% `data` - double array.
+function data = fload(fp, hl)
+    if ~exist('hl','var'), hl = 0; end
+    assert((hl >= 0) && (hl == round(hl)), 'Number of header lines is incorrect.');
+    assert(exist(fp, 'file'), 'File "%s" does not exist.', fp);
     fid = fopen(fp, 'r');
-    assert(fid ~= -1, 'File %s can not be read.', fp);
+    assert(fid ~= -1, 'File "%s" can not be read.', fp);
     cuo = onCleanup(@() fclose(fid));
-    %% columns number
-    % skipping of header lines
-    for k = 1:hl, fgetl(fid); end
-    tline = fgetl(fid);
-    [~, acols] = sscanf(tline, '%f');
-    data_wo_blanks = reshape(cell2mat(textscan(fid, '%f')), acols, [])';
-    %% memory pre-allocation for array
-    a = zeros(size(data_wo_blanks,1)+1, acols);
-    %% array formation
-    a(1,:)     = sscanf(tline, '%f');
-    a(2:end,:) = data_wo_blanks;
+    for k = 1:hl, fgetl(fid); end % skipping of header lines
+    acols = 0;
+    while acols == 0
+      tline = fgetl(fid);
+      [~, acols] = sscanf(tline, '%f');
+    end
+    c = textscan(fid, '%f', 'CommentStyle', '%');
+    data_wo_blanks = reshape(cell2mat(c), acols, [])';
+    data          = zeros(size(data_wo_blanks,1)+1, acols);
+    data(1,    :) = sscanf(tline, '%f');
+    data(2:end,:) = data_wo_blanks;
 end
